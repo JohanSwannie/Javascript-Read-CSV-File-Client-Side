fe986a316ce2343403a699b74999df4104939586
jswan <jswannieman@gmail.com>
2023-09-16 10:48:11 +1200

    Update read-csv-file Javascript Component

--- read-csv-file.js
+++ read-csv-file.js
@@ -1,7 +1,7 @@
 const diffy = document.querySelector(".container");
 
 fetchCSVFiles()
-  .then((response) => console.log(`The CSV File fetch was Successful`))
+  .then((response) => console.log("Hallo"))
   .catch((error) =>
     console.error(`There was an error with fetching the CSV File - ${error}`)
   );
@@ -13,9 +13,22 @@ async function fetchCSVFiles() {
   for (let row of rows) {
     let columnFields = row.split(",");
     let htmlText = "";
-    for (let entry of columnFields) {
-      htmlText = htmlText + entry + "  -  ";
+    let tArr = [];
+
+    // for (let entry of columnFields) {
+    //   htmlText = htmlText + entry + "  -  ";
+    // }
+
+    // for (let i = 0; i < columnFields.length; i++) {
+    //   tArr.push(columnFields[i]);
+    // }
+
+    for (let val of columnFields) {
+      tArr.push(val);
     }
+
+    htmlText = `${tArr[0]} of age ${tArr[1]} is a ${tArr[2]} with email address ${tArr[3]} and lives in ${tArr[4]}`;
+
     const para = document.createElement("p");
     const text = document.createTextNode(htmlText);
     para.appendChild(text);
